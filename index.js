3fdf9b02b60603d63f4b3c9ca77902452df7d35d
Raju <ashfaqraju27@gmail.com>
2023-04-20 15:49:52 +0600

    new users email & name saved

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run(){
     try{
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
         
         //use Aggregate to query multiple collection & then merge data
         app.get('/appointmentOptions', async(req, res) =>{
@@ -100,7 +101,12 @@ async function run(){
             res.send(options);
         }) */
 
-
+        app.get('/bookings', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings); 
+        })
 
         app.post('/bookings', async(req, res) =>{
             const booking = req.body;
@@ -119,6 +125,12 @@ async function run(){
             }
 
             const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
     }
