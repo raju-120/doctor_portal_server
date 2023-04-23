9b6dc8a0c1e7a106fe95f83a3ad7a10b89d220c1
Raju <ashfaqraju27@gmail.com>
2023-04-24 01:13:53 +0600

    admin role added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -169,9 +169,37 @@ async function run(){
             res.send(users);
         });
 
+        app.get('/users/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin : user?.role === 'admin'});
+        })
+
         app.post('/users', async(req, res) =>{
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.put('/users/admin/:id',verifyJWT, async(req, res) =>{
+            const decodedEmail = req.decoded.email;
+            const query = {email: decodedEmail}
+            const user = await usersCollection.findOne(query);
+            if(user?.role !== 'admin'){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+
+
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role : 'admin'
+                }
+            }
+            const result =  await usersCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
     }
