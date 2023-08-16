b3de4e6645a31d7ba16a386d8194336351b627d8
Raju <ashfaqraju27@gmail.com>
2023-08-16 18:30:28 +0600

    user deleted control handled

--- index.js
+++ index.js
@@ -272,32 +272,44 @@ async function run(){
             res.status(403).send({accessToken: 'jwt'});
         })
 
+        //All Users
+
         app.get('/users', async(req, res)=>{
             const query ={};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
         });
-
-        app.get('/users/admin/:email', async(req, res) =>{
-            const email = req.params.email;
-            const query = { email };
-            const user = await usersCollection.findOne(query);
-            res.send({isAdmin : user?.role === 'admin'});
-        })
-
         app.post('/users', async(req, res) =>{
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
 
+        app.delete('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //Admin section
+
+        app.get('/users/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin : user?.role === 'admin'});
+        });
+
         app.put('/users/admin/:id', async(req, res) =>{
-            const decodedEmail = req.decoded.email;
+            /* const decodedEmail = req.decoded.email;
+            console.log('Check in', decodedEmail);
             const query = {email: decodedEmail}
             const user = await usersCollection.findOne(query);
+            
             if(user?.role !== 'admin'){
                 return res.status(403).send({message: 'forbidden access'})
-            }
+            } */
 
 
             const id = req.params.id;
@@ -309,8 +321,15 @@ async function run(){
                 }
             }
             const result =  await usersCollection.updateOne(filter, updatedDoc, options);
+            console.log(result);
             res.send(result);
-        })
+        });
+
+        
+
+        
+
+        
 
         //temporary to update price field appointment option
 
